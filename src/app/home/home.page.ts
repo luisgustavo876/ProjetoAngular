a937fc66b3d94a7e4bb502bedc1f5f42a6339eef
Luís Gustavo <107196733+luisgustavo876@users.noreply.github.com>
2023-05-04 17:23:51 +0000

    tfggv

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,9 +7,10 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,CommonModule],
 })
 export class HomePage {
+  item_selecionado : string = 'desenvolvimento_sistemas';
   trocouCurso(event:any){
     console.log(event.detail.value);
   }
