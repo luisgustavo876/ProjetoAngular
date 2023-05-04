eb91517446a19f1b1c4e585f8c5c8f230d1ed9a7
Luís Gustavo <107196733+luisgustavo876@users.noreply.github.com>
2023-05-04 17:51:17 +0000

    dapora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,14 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule , CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  produto = [
+    {id : 0, 'nome' : 'PS5', 'preco' :5000 },
+    {id : 1, 'nome' : 'PS4', 'preco' :2000 },
+    {id : 2, 'nome' : 'super nes', 'preco' :250 },
+    {id : 3, 'nome' : 'polistation', 'preco' :800 },
+    {id : 4, 'nome' : 'rapberry', 'preco' :150 },
+  ]
 }
